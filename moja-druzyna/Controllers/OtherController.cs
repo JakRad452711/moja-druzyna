6843ec39bff96d2fd242ee626eacfd576e65514a
s426818 <pauszy10@st.amu.edu.pl>
2022-06-02 23:00:38 +0200

    make settings view

---
+++ moja-druzyna/Controllers/OtherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using moja_druzyna.Data;
+using moja_druzyna.Models;
+using System.Diagnostics;
+
+namespace moja_druzyna.Controllers
+{
+    public class OtherController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<OtherController> _logger;
+
+        public OtherController(ApplicationDbContext dbContext, ILogger<OtherController> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public IActionResult Settings()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return Redirect("/Identity/Account/Login");
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
