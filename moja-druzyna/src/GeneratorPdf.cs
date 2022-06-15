8c3a45a85dd529f1104c5d5284939dd226a7508e
s407409 <grzgap@st.amu.edu.pl>
2022-06-15 22:28:19 +0200

    Adding GeneratorPDF class

---
+++ moja-druzyna/src/GeneratorPdf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System.Drawing;
+
+namespace moja_druzyna.src
+{
+    public class GeneratorPdf
+    {
+        public void CreateTestPdf()
+        {
+            PdfDocument doc = new PdfDocument();
+
+            PdfPageBase page = doc.Pages.Add();
+
+            page.Canvas.DrawString("Hello World",
+                new PdfFont(PdfFontFamily.Helvetica, 13f),
+                new PdfSolidBrush(Color.Black),
+                new PointF(50, 50));
+
+            doc.SaveToFile("Output.pdf");
+        }
+    }
+}
