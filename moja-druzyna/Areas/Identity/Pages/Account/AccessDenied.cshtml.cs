9419dc254696f1d150f9841aceffb67c41d8d92e
Jakub Danilewicz <jdanilewicz123@gmail.com>
2022-06-23 19:23:21 +0200

    Changes in login n registration

---
+++ moja-druzyna/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace moja_druzyna.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
